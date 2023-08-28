739962e25e1ec2b0e700103795c79e65a35d0a07
Vikky Thute <vickythute76@gmail.com>
2023-08-28 13:39:05 +0530

    unknown Commit

---
+++ vfc-website/src/Components/Header.js
@@ -0,0 +1,36 @@
+import React, { Fragment } from "react";
+
+import { Navbar, NavbarBrand} from 'reactstrap'
+
+const Header=()=>{
+    return(
+        <Fragment>
+
+            {/* <div className="card">
+                <h1>hwlo</h1>
+            </div>
+             <div className="card" style={{background:"linear-gradient(to bottom, Transparente 0%,Transparente 50%,red 50%,red 100%)"}}/> */}
+{/* <div className="card" style={{background: "linear-gradient(to bottom, Transparente 0%,Transparente 50%,red 50%,red 100%)" ,height:500}}/> */}
+
+
+
+  {/* <Navbar
+    className="my-2"
+    color="primary"
+    dark
+  >
+    <NavbarBrand href="/">
+      <img
+        alt="logo"
+        src="../image/vfc_logo.png"
+        style={{
+          width: 250
+        }}
+      />
+      Victorious Fencing Club
+    </NavbarBrand>
+  </Navbar> */}
+         </Fragment>
+    )
+}
+export {Header}
